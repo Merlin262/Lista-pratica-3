13b327eec3c85dcd909d2dbbb2354f13190dd891
João Merlin <88944432+Merlin262@users.noreply.github.com>
2022-10-03 19:40:41 -0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,9 @@
+i=0
+num=0
+soma=0
+
+for i in range(50):
+    if (i%2==0):
+        soma+=i*2
+        num+=1
+print(soma)
---
+++ 9.py
@@ -0,0 +1,6 @@
+lista=[]
+
+for i in range(10):
+    lista.append(float(input('Numero '+ str(i+1) + ':\n')))
+
+print(lista)
